3181dfe02a865ce7108adb27ab6809d3c1dc8cb1
James <barowikajames@gmail.com>
2024-07-10 00:18:29 +0100

    removed: kde default applications

--- main.sh
+++ main.sh
@@ -2,3 +2,24 @@ sudo apt install nala
 
 sudo nala update
 sudo nala upgrade -y
+
+sudo nala remove firefox-esr -y
+sudo nala remove korganizer -y
+sudo nala remove gwenview -y
+sudo nala remove kontrast -y
+sudo nala remove akregator -y
+sudo nala remove kdeconnect -y
+sudo nala remove kmail -y
+sudo nala remove imagemagick-6.q16 -y
+sudo nala remove konqueror -y
+sudo nala remove dragonplayer -y
+sudo nala remove juk -y
+sudo nala remove kwalletmanager -y
+sudo nala remove xterm -y
+sudo nala remove knotes -y
+sudo nala remove kaddressbook -y
+sudo nala remove kmouth -y
+sudo nala remove kwrite -y
+sudo nala remove kmousetool -y
+sudo nala remove kmag -y
+sudo nala remove sweeper -y
