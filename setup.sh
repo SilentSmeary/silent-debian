c7357a3bdfa18ab84fc87f4bdaab3e76a59d289f
James <barowikajames@gmail.com>
2024-07-18 17:41:09 +0100

    update: vscode installation

--- setup.sh
+++ setup.sh
@@ -23,21 +23,24 @@ sudo nala remove kwrite -y
 sudo nala remove kmousetool -y
 sudo nala remove kmag -y
 sudo nala remove sweeper -y
+sudo nala remove kate -y
 
-cd Downloads/
+sudo mkdir local-downloads/
+cd local-downloads/
+
 sudo wget "https://github.com/Alex313031/thorium/releases/download/M124.0.6367.218/thorium-browser_124.0.6367.218_AVX2.deb"
 sudo nala install fonts-liberation -y
 sudo nala install libu2f-udev -y
 sudo dpkg -i thorium-browser_124.0.6367.218_AVX2.deb
 
-sudo wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+sudo wget "https://vscode.download.prss.microsoft.com/dbazure/download/stable/f1e16e1e6214d7c44d078b1f0607b2388f29d729/code_1.91.1-1720564633_amd64.deb"
 sudo dpkg -i code_1.91.1-1720564633_amd64.deb
 
 sudo nala install git
 
 sudo wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
-sudo nala update -y
+sudo nala update
 sudo nala install github-desktop -y
 
 echo "[###]   Nala Installed"
