453145b97d26ca38ce23a318a495158253c36f38
James <barowikajames@gmail.com>
2024-08-23 18:52:39 +0100

    code refactor

--- setup.sh
+++ setup.sh
@@ -5,23 +5,18 @@ sudo nala upgrade -y
 
 sudo nala remove firefox-esr zutty kate sweeper kmag kmousetool kwrite kmouth kaddressbook knotes xterm kwalletmanager juk dragonplayer konqueror imagemagick-6.q16 kmail kdeconnect akregator kontrast gwenview korganizer -y
 
+sudo nala install curl fonts-liberation libu2f-udev git gh -y
+
 cd #
 sudo rm -rf local-downloads/
 sudo mkdir local-downloads/
 cd local-downloads/
 
-sudo nala install curl -y
-
 sudo wget "https://github.com/Alex313031/thorium/releases/download/M124.0.6367.218/thorium-browser_124.0.6367.218_AVX2.deb"
-sudo nala install fonts-liberation -y
-sudo nala install libu2f-udev -y
 sudo dpkg -i thorium-browser_124.0.6367.218_AVX2.deb
 
 sudo wget "https://vscode.download.prss.microsoft.com/dbazure/download/stable/f1e16e1e6214d7c44d078b1f0607b2388f29d729/code_1.91.1-1720564633_amd64.deb"
 sudo dpkg -i code_1.91.1-1720564633_amd64.deb
-
-sudo nala install git -y 
-sudo nala install gh -y
 
 sudo wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
