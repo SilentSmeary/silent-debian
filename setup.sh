549d174f690f9106dbb91d58b0f76b13feaf825d
James <barowikajames@gmail.com>
2024-08-23 22:58:14 +0100

    second try at fix

--- setup.sh
+++ setup.sh
@@ -32,7 +32,6 @@ sudo wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Mesl
 sudo mv Meslo.zip /usr/share/fonts
 cd #
 cd /usr/share/fonts
-sudo unzip Meslo.zip
 fc-cache -fv
 
 cd #
