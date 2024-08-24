6c320f41f1bf750fa576b8130b31fc342904ab45
James <barowikajames@gmail.com>
2024-08-24 17:00:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,28 +29,20 @@ sudo nala update
 sudo nala install github-desktop -y
 
 FONT_NAME="MesloLGS Nerd Font Mono"
-    if fc-list :family | grep -iq "$FONT_NAME"; then
-        echo "Font '$FONT_NAME' is installed."
-    else
-        echo "Installing font '$FONT_NAME'"
-        # Change this URL to correspond with the correct font
-        FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip"
-        FONT_DIR="$HOME/.local/share/fonts"
-        # check if the file is accessible
-        if wget -q --spider "$FONT_URL"; then
-            TEMP_DIR=$(mktemp -d)
-            wget -q --show-progress $FONT_URL -O "$TEMP_DIR"/"${FONT_NAME}".zip
-            unzip "$TEMP_DIR"/"${FONT_NAME}".zip -d "$TEMP_DIR"
-            mkdir -p "$FONT_DIR"/"$FONT_NAME"
-            mv "${TEMP_DIR}"/*.ttf "$FONT_DIR"/"$FONT_NAME"
-            # Update the font cache
-            fc-cache -fv
-            # delete the files created from this
-            rm -rf "${TEMP_DIR}"
-            echo "'$FONT_NAME' installed successfully."
-        else
-            echo "Font '$FONT_NAME' not installed. Font URL is not accessible."
-        fi
+echo "Installing font '$FONT_NAME'"
+# Change this URL to correspond with the correct font
+FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip"
+FONT_DIR="$HOME/.local/share/fonts"
+TEMP_DIR=$(mktemp -d)
+wget -q --show-progress $FONT_URL -O "$TEMP_DIR"/"${FONT_NAME}".zip
+unzip "$TEMP_DIR"/"${FONT_NAME}".zip -d "$TEMP_DIR"
+mkdir -p "$FONT_DIR"/"$FONT_NAME"
+mv "${TEMP_DIR}"/*.ttf "$FONT_DIR"/"$FONT_NAME"
+# Update the font cache
+fc-cache -fv
+# delete the files created from this
+rm -rf "${TEMP_DIR}"
+echo "'$FONT_NAME' installed successfully."
 
 cd #
 cd local-downloads
