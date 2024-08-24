80833b39a5aea5e222d95b00a6bea1da476c0018
James <barowikajames@gmail.com>
2024-08-24 20:38:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -78,21 +78,21 @@ sudo nala install virt-manager -y
 # sudo mv kitty.conf ~/.config/kitty/
 # sudo mv nord.conf ~/.config/kitty/
 
-    echo "Install Kitty if not already installed..."
-    if ! command_exists kitty; then
-            sudo nala install -y kitty
-            ;;
-        esac
-    else
-        echo "Kitty is already installed."
-    fi
-    echo "Copy Kitty config files"
-    if [ -d "${HOME}/.config/kitty" ]; then
-        cp -r "${HOME}"/.config/kitty "${HOME}"/.config/kitty-bak
-    fi
-    mkdir -p "${HOME}"/.config/kitty/
-    wget -O "${HOME}"/.config/kitty/kitty.conf https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/kitty/kitty.conf
-    wget -O "${HOME}"/.config/kitty/nord.conf https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/kitty/nord.conf
+echo "Install Kitty if not already installed..."
+if ! command_exists kitty; then
+        sudo nala install -y kitty
+        ;;
+    esac
+else
+    echo "Kitty is already installed."
+fi
+echo "Copy Kitty config files"
+if [ -d "${HOME}/.config/kitty" ]; then
+    cp -r "${HOME}"/.config/kitty "${HOME}"/.config/kitty-bak
+fi
+mkdir -p "${HOME}"/.config/kitty/
+wget -O "${HOME}"/.config/kitty/kitty.conf https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/kitty/kitty.conf
+wget -O "${HOME}"/.config/kitty/nord.conf https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/kitty/nord.conf
 
 cd #
 
