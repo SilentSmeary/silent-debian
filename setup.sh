eb762050d52e6a9e5728daf16731cb9e09db5761
James <barowikajames@gmail.com>
2024-08-24 20:37:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -64,19 +64,35 @@ sudo dpkg -i ProtonPass_1.22.1.deb
 
 sudo nala install virt-manager -y
 
-sudo nala install kitty -y
+# sudo nala install kitty -y
 
-cd #
-cd .config/kitty
+# cd #
+# cd .config/kitty
 
-# No need to remove files as mv will replace them
-cd ~/local-downloads/
+# # No need to remove files as mv will replace them
+# cd ~/local-downloads/
 
-sudo wget https://raw.githubusercontent.com/SilentSmeary/silent-debian/main/config/kitty/kitty.conf
-sudo wget https://raw.githubusercontent.com/SilentSmeary/silent-debian/main/config/kitty/nord.conf
+# sudo wget https://raw.githubusercontent.com/SilentSmeary/silent-debian/main/config/kitty/kitty.conf
+# sudo wget https://raw.githubusercontent.com/SilentSmeary/silent-debian/main/config/kitty/nord.conf
 
-sudo mv kitty.conf ~/.config/kitty/
-sudo mv nord.conf ~/.config/kitty/
+# sudo mv kitty.conf ~/.config/kitty/
+# sudo mv nord.conf ~/.config/kitty/
+
+    echo "Install Kitty if not already installed..."
+    if ! command_exists kitty; then
+            sudo nala install -y kitty
+            ;;
+        esac
+    else
+        echo "Kitty is already installed."
+    fi
+    echo "Copy Kitty config files"
+    if [ -d "${HOME}/.config/kitty" ]; then
+        cp -r "${HOME}"/.config/kitty "${HOME}"/.config/kitty-bak
+    fi
+    mkdir -p "${HOME}"/.config/kitty/
+    wget -O "${HOME}"/.config/kitty/kitty.conf https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/kitty/kitty.conf
+    wget -O "${HOME}"/.config/kitty/nord.conf https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/kitty/nord.conf
 
 cd #
 
