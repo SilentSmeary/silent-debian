7a6de7f68f50dd3bbccac0603ee191a9f1cf5e27
James <barowikajames@gmail.com>
2024-09-03 17:55:07 +0100

    added xampp.desktop

--- xampp.sh
+++ xampp.sh
@@ -3,5 +3,6 @@ cd local-downloads
 sudo wget -O xampp.png https://commons.wikimedia.org/wiki/File:XAMPP_Logo.png
 sudo mv xampp.png /opt/lampp/img/
 
+sudo wget https://raw.githubusercontent.com/SilentSmeary/silent-debian/main/xampp.desktop
 sudo mv local-downloads/xampp.desktop /usr/share/applications/
 sudo chmod +x /usr/share/applications/xampp.desktop
